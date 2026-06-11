3062dd7d8f6e12fe49240299912daac30698edb7
wenweia <202365934+wenweia@users.noreply.github.com>
2026-06-11 14:38:38 -0300

    Fix r77 description length

--- cogs/events/returns/week_77.py
+++ cogs/events/returns/week_77.py
@@ -222,7 +222,7 @@ class _NominateModal(discord.ui.Modal):
     )
     title_input = discord.ui.TextInput(
         label="Possible Award (Suggested Title)",
-        placeholder="Succinctly describe this user's positive contributions to your experience",
+        placeholder="Succinctly charactarise their positive contributions!",
         style=discord.TextStyle.short,
         min_length=1,
         max_length=MAX_TITLE_LEN,
